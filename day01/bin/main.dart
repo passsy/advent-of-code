710767ab7276720a8f86ea30a754551c2e23eb64
Pascal Welsch <pascal@welsch.dev>
2019-12-01 18:59:08 +0100

    Cleanup solution

--- day01/bin/main.dart
+++ day01/bin/main.dart
@@ -1,22 +1,12 @@
 import 'dart:io';
-import 'package:dartx/dartx.dart';
 
 import 'package:day01/day01.dart';
+import 'package:dartx/dartx.dart';
 
 void main(List<String> arguments) {
   final lines = File('input.txt').readAsStringSync().split('\n');
+  final masses = lines.map((it) => int.parse(it.trim()));
 
-  final all = lines.map((it) {
-    final mass = int.parse(it.trim());
-    return fuel(mass);
-  });
-
-  print("Part 1 - ${all.sum()}");
-
-  final all2 = lines.map((it) {
-    final mass = int.parse(it.trim());
-    return recursiveFuel(mass);
-  });
-
-  print("Part 2 - ${all2.sum()}");
+  print("Part 1 - ${masses.map((it) => fuel(it)).sum()}");
+  print("Part 1 - ${masses.map((it) => recursiveFuel(it)).sum()}");
 }
