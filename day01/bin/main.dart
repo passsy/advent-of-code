9b53bb3de57269683b76b21862b07f8be85a188a
Pascal Welsch <pascal@welsch.dev>
2019-12-01 21:30:28 +0100

    Fix output

--- day01/bin/main.dart
+++ day01/bin/main.dart
@@ -8,5 +8,5 @@ void main(List<String> arguments) {
   final masses = lines.map((it) => int.parse(it.trim()));
 
   print("Part 1 - ${masses.map((it) => fuel(it)).sum()}");
-  print("Part 1 - ${masses.map((it) => recursiveFuel(it)).sum()}");
+  print("Part 2 - ${masses.map((it) => recursiveFuel(it)).sum()}");
 }
