50811e3edf7f50944fec47e4aa86ad1528f4b3fd
Pascal Welsch <pascal@welsch.dev>
2019-12-02 23:48:24 +0100

    Solve solution 2

--- day02/bin/main.dart
+++ day02/bin/main.dart
@@ -4,12 +4,30 @@ import 'package:aoc_common/aoc_common.dart';
 import 'package:day02/day02.dart';
 
 void main(List<String> arguments) {
-  final numbers = File('input.txt').readAsStringSync().split(',').map((it) => it.toInt()).toList().immutable();
+  final program = File('input.txt').readAsStringSync().split(',').map((it) => it.toInt()).toList().immutable();
 
-  final program1 = numbers.toMutableList();
-  program1[1] = 12;
-  program1[2] = 2;
-  final interpreter = Interpreter(program1);
-  interpreter.run();
+  KtList<int> reproduceCrash(int noun, int verb) {
+    final p = program.toMutableList();
+    p[1] = noun;
+    p[2] = verb;
+    return p.toList();
+  }
+
+  final interpreter = Interpreter(reproduceCrash(12, 2))..run();
   print("Part 1 - ${interpreter.memoryDump[0]}");
+
+  final nouns = 0.rangeTo(99);
+  final verbs = 0.rangeTo(99);
+
+  for (final noun in nouns) {
+    for (final verb in verbs) {
+      final interpreter = Interpreter(reproduceCrash(noun, verb))..run();
+      final result = interpreter.memoryDump[0];
+      if (result == 19690720) {
+        print("Part 2 - ${100 * noun + verb} (noun=$noun verb=$verb)");
+        return;
+      }
+    }
+  }
+  print("No solution found");
 }
