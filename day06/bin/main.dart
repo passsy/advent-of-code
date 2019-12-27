090eaa9898916d6ffb010d6c3fd13025f2e066e9
Pascal Welsch <pascal@welsch.dev>
2019-12-27 14:17:38 +0100

    Add day6

---
+++ day06/bin/main.dart
@@ -0,0 +1,8 @@
+import 'package:day06/day06.dart';
+import 'package:aoc_common/aoc_common.dart';
+
+void main(List<String> arguments) {
+
+  print("Part 1 - ${part1()}");
+  print("Part 2 - ${part2()}");
+}
